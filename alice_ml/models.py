91d0ffcd5453597de338ebbaa0801da41aa5b948
NadezhdaAlsahanova <72088271+NadezhdaAlsahanova@users.noreply.github.com>
2023-03-07 16:30:36 +0300

    Models on pmml

--- alice_ml/models.py
+++ alice_ml/models.py
@@ -2,12 +2,19 @@ import importlib.resources as pkg_resources
 import joblib
 
 import pandas as pd
+import numpy as np
 
 from alice_ml.features import get_features_from_mne
 from alice_ml import pretrained
+from sklearn_pmml_model.ensemble import PMMLGradientBoostingClassifier 
+from sklearn_pmml_model.linear_model import PMMLLogisticRegression
 
+<<<<<<< Updated upstream
 
 def predict_mne(raw, ica, model = 'lr', flags = 'all'):
+=======
+def predict_mne(raw, ica, model = 'LR', features = 'all', flags = 'all'):
+>>>>>>> Stashed changes
    
     """
     Using of pretrained model on a raw data to predict ICA label via machine learning techniques
@@ -23,21 +30,38 @@ def predict_mne(raw, ica, model = 'lr', flags = 'all'):
     flags : str or list of flags
     What flags to predict, by default "all" - predicting all available flags ('flag_brain', 'flag_alpha', 'flag_mu', 'flag_muscles', 'flag_eyes', 'flag_heart', 'flag_ch_noise')
     """
+<<<<<<< Updated upstream
     
     features_df = get_features_from_mne(raw, ica)
 
     scaler = joblib.load(pkg_resources.open_binary(pretrained, 'scaler.joblib'))
+=======
+    features_df = get_features_from_mne(raw, ica)    
+>>>>>>> Stashed changes
 
     models = {}
+    ideces = {}
     if flags == 'all':
-        flags = ['flag_brain', 'flag_muscles', 'flag_eyes']
+        flags = ['flag_mu', 'flag_alpha', 'flag_eyes']
     for flag in flags:
-        models[flag] = joblib.load(pkg_resources.open_binary(pretrained, f'{model}_' + flag + '.joblib'))
+        if model == 'LR':
+            models[flag] = PMMLLogisticRegression(pmml=pkg_resources.open_binary(pretrained, f"{model}_{flag}.pmml"))
+            ideces[flag] = np.arange(17)
+        elif model == 'XGB':
+            models[flag] = PMMLGradientBoostingClassifier(pmml=pkg_resources.open_binary(pretrained, f"{model}_{flag}.pmml"))
+            ideces[flag] = np.array([int(x[1:])-1 for x in list(models[flag].fields.keys())[1:]])
+            
+    scalers = {}
+    for flag in flags:
+        if flag=='flag_eyes':
+            scalers[flag] = joblib.load(pkg_resources.open_binary(pretrained, 'scaler_eyes.joblib'))
+        else:
+            scalers[flag] = joblib.load(pkg_resources.open_binary(pretrained, 'scaler_mu_alpha.joblib'))
     
-    X = scaler.transform(features_df)
     cols = {}
     for flag in flags:
-        cols[flag] = models[flag].predict_proba(X)[:, 1]
+        X = scalers[flag].transform(features_df)
+        cols[flag] = models[flag].predict_proba(X[:,ideces[flag]])[:, 1]
     
     pred_df = pd.DataFrame(cols, index=features_df.index)
     return pred_df
